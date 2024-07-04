ca1ee671af29e498701ca52f456c2b8d54de2178
Onur Nergiz <you@example.com>
2024-07-04 13:37:05 +0300

    xml cekme

--- KurSite/Controllers/HomeController.cs
+++ KurSite/Controllers/HomeController.cs
@@ -3,6 +3,14 @@ using System.Collections.Generic;
 using System.Linq;
 using System.Web;
 using System.Web.Mvc;
+using System.Threading.Tasks;
+using System.Net.Http;
+using System.Xml.Linq;
+using System.Xml.Serialization;
+using System.Security.Cryptography.X509Certificates;
+using KurSite.Models;
+using System.Xml;
+using Microsoft.SqlServer.Server;
 
 namespace KurSite.Controllers
 {
@@ -11,7 +19,13 @@ namespace KurSite.Controllers
         // GET: Home
         public ActionResult Index()
         {
-            return View();
+            string link = "https://www.tcmb.gov.tr/kurlar/today.xml";
+            var xmlDocument = new XmlDocument();
+            xmlDocument.Load(link);
+            string usd = xmlDocument.SelectSingleNode("Tarih_Date/Currency[@Kod=\"USD\"]/BanknoteBuying").InnerXml.ToString();
+
+            return View(usd);
+
         }
     }
 }
---
+++ KurSite/Models/Kur.cs
@@ -0,0 +1,29 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Xml.Serialization;
+
+
+namespace KurSite.Models
+{
+    [XmlRoot("Kur")]
+    public class Kur
+    {
+        [XmlElement("ForexCode")]
+        public string ForexCode { get; set; }
+
+        [XmlElement("Name")]
+        public string Name { get; set; }
+
+        [XmlElement("ForexBuying")]
+        public string  ForexBuying{ get; set; }
+
+        [XmlElement("ForexSelling")]
+        public string ForexSelling { get; set; }
+
+
+
+
+    }
+}
