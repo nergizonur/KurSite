239dd2e0202426253a9d79c700d295441bea3c73
Yusuf Halil ÇATAL <y.halilc.37@gmail.com>
2024-07-05 08:48:34 +0300

    Helpers

---
+++ KurSite/Helpers/CurrencyCodes.cs
@@ -0,0 +1,37 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+
+namespace KurSite.Helpers
+{
+    public class CurrencyCodes
+    {
+        private string[] codes = {
+            "USD",
+            "AUD",
+            "DKK",
+            "EUR",
+            "GBP",
+            "CHF",
+            "SEK",
+            "CAD",
+            "KWD",
+            "NOK",
+            "SAR",
+            "JPY",
+            "BGN",
+            "RON",
+            "RUB",
+            "IRR",
+            "CNY",
+            "PKR",
+            "QAR",
+            "KRW",
+            "AZN",
+            "AED",
+
+        };
+        public string[] getCodes() { return codes; }
+    }
+}
