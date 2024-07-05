23af8b037c9626ec65234e32a30fa4a8e41d5e68
Onur Nergiz <you@example.com>
2024-07-05 10:43:56 +0300

    tarih secim

--- KurSite/Controllers/HomeController.cs
+++ KurSite/Controllers/HomeController.cs
@@ -12,12 +12,14 @@ using KurSite.Models;
 using System.Xml;
 using Microsoft.SqlServer.Server;
 using KurSite.Services;
+using KurSite.Helpers;
 
 namespace KurSite.Controllers
 {
     public class HomeController : Controller
     {
         ForexServices forexService = new ForexServices();
+        DateConverter dateConverter = new DateConverter();
 
         // GET: Home
 
@@ -31,6 +33,31 @@ namespace KurSite.Controllers
         public ActionResult Index(string code)
         {
             return View(forexService.getForexList(code));
+        }
+
+        [HttpPost]
+        public ActionResult SelectDate(string date)
+        {
+
+            
+            
+            CurrencyCodes currencyCode = new CurrencyCodes();
+            List<Kur> forexList = new List<Kur>();
+            string link = $"https://www.tcmb.gov.tr/kurlar/{dateConverter.getYear(date)}{dateConverter.getMonth(date)}/{dateConverter.getDay(date)}{dateConverter.getMonth(date)}{dateConverter.getYear(date)}.xml";
+            var xmlDocument = new XmlDocument();
+            xmlDocument.Load(link);
+            foreach (var code in currencyCode.getCodes())
+            {
+                Kur forex = new Kur();
+                forex.ForexCode = code;
+                forex.ForexName = xmlDocument.SelectSingleNode($"Tarih_Date/Currency[@Kod=\"{code}\"]/Isim").InnerXml;
+                forex.ForexBuying = xmlDocument.SelectSingleNode($"Tarih_Date/Currency[@Kod=\"{code}\"]/ForexBuying").InnerXml;
+                forex.ForexSelling = xmlDocument.SelectSingleNode($"Tarih_Date/Currency[@Kod=\"{code}\"]/ForexSelling").InnerXml;
+                forexList.Add(forex);
+            }
+
+            return View("Index",forexList);
+
         }
     }
 }
---
+++ KurSite/Helpers/DateConverter.cs
@@ -0,0 +1,38 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Runtime.InteropServices;
+using System.Web;
+
+namespace KurSite.Helpers
+{
+    public class DateConverter
+    {
+        public string getDay(string date)
+        {
+            //2024-05-01
+            string day = "";
+            int lenght = date.Length;
+             day=date.Substring(lenght-2, 2);
+            return day;
+        }
+
+        public string getMonth(string date)
+        {
+            //2024-05-1
+            string month = "";
+            int lenght = date.Length;
+            month = date.Substring(lenght -5 , 2);
+            return month;
+        }
+
+        public string getYear(string date)
+        {
+            //2024-05-1
+            string year = "";
+            int lenght = date.Length;
+            year = date.Substring(lenght - 10, 4);
+            return year;
+        }
+    }
+}
