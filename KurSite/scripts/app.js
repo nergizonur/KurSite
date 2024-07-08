9a954c3d759c62f6cf978cf7884885b20a0b969b
Yusuf Kurt <ykurt8205@gmail.com>
2024-07-08 14:24:27 +0300

    jquery xml veri çekme

--- KurSite/scripts/app.js
+++ KurSite/scripts/app.js
@@ -4,23 +4,17 @@ $(function () {
         $("td").css("color", colorValue);
     });
 
+    $("#fontSizeChangeButton").click(function () {
+        var fontSize = $("#fontSize").val();
+        if (fontSize < 12 || fontSize > 30) {
+            alert("Lütfen 12 ile 30 arasýnda bir deðer giriniz!");
+        } else {
+            var fontSizeValue = fontSize + "px";
+            $("td").css("fontSize", fontSizeValue);
+        }
+    });
 
-
-
-    $("#fontSizeChangeButton").click(
-        function () {
-            var fontSize = $("#fontSize").val();
-            if (fontSize < 12 || fontSize > 30) {
-                alert("Lutfen 12 ile 30 arasinda bir deger giriniz!")
-            }
-            else {
-                var fontSizeValue = fontSize + "px";
-                $("td").css("fontSize", fontSizeValue);
-            }
-            
-        });
     $("body").on("load", function () {
-        
         $.ajax({
             type: "GET",
             url: "https://www.tcmb.gov.tr/kurlar/202310/04102023.xml",
@@ -31,7 +25,7 @@ $(function () {
                 $(xml).find('Currency').each(function () {
                     var baslik = $(this).find('Isim').text();
                     var yazar = $(this).find('ForexBuying').text();
-                    console.log("Baþlýk: " + baþlýk + ", Yazar: " + yazar);
+                    console.log("Baþlýk: " + baslik + ", Yazar: " + yazar);
                 });
             },
             error: function () {
@@ -39,6 +33,4 @@ $(function () {
             }
         });
     });
-    
-    
 });
