3e2b8a59c7937353ced59abe0e6d52b752e42ed2
Onur Nergiz <you@example.com>
2024-07-08 14:21:06 +0300

    jquery xml

--- KurSite/scripts/app.js
+++ KurSite/scripts/app.js
@@ -4,10 +4,13 @@ $(function () {
         $("td").css("color", colorValue);
     });
 
+
+
+
     $("#fontSizeChangeButton").click(
         function () {
             var fontSize = $("#fontSize").val();
-            if (fontSize < 12 || fontSize > 31) {
+            if (fontSize < 12 || fontSize > 30) {
                 alert("Lutfen 12 ile 30 arasinda bir deger giriniz!")
             }
             else {
@@ -15,6 +18,27 @@ $(function () {
                 $("td").css("fontSize", fontSizeValue);
             }
             
+        });
+    $("body").on("load", function () {
+        
+        $.ajax({
+            type: "GET",
+            url: "https://www.tcmb.gov.tr/kurlar/202310/04102023.xml",
+            dataType: "xml",
+            success: function (xml) {
+                debugger;
+                // XML içindeki kitaplarý iþle
+                $(xml).find('Currency').each(function () {
+                    var baslik = $(this).find('Isim').text();
+                    var yazar = $(this).find('ForexBuying').text();
+                    console.log("Baþlýk: " + baþlýk + ", Yazar: " + yazar);
+                });
+            },
+            error: function () {
+                console.log("XML dosyasý iþlenirken bir hata oluþtu.");
+            }
+        });
     });
+    
     
 });
