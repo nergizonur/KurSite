5e01d44ab4ebc50e3f1efb8e3e2822777ded7f1d
Onur Nergiz <nerg.onur@gmail.com>
2024-09-16 17:10:46 +0300

    excel indirme ve js ile kur çevirme

--- KurSite/scripts/home/exchange.js
+++ KurSite/scripts/home/exchange.js
@@ -3,11 +3,18 @@
 
 
     $("#exchangeButton").click(function () {
-        var firstForex = $("#firstForexSelect").val();
-        var secondForex = $("#secondForexSelect").val();
+        var firstForex = $("#firstForexSelected option:selected").text();
+        var secondForex = $("#secondForexSelected option:selected").text();
+
+        var firstForexValue = $("#firstForexSelected").val();
+        var secondForexValue = $("#secondForexSelected").val();
+        var number = $("#number").val();
+
+        var value = (firstForexValue / secondForexValue)*number;
+
         $("#firstForex").text(firstForex)  ;
         $("#secondForex").text(secondForex)  ;
-
+        $("#forexValue").text(value);
 
 
     })
---
+++ KurSite/scripts/home/exportToExcel.js
@@ -0,0 +1,5 @@
+﻿function downloadToExcel() {
+    var table = document.getElementById("forexTable");
+    var workbook = XLSX.utils.table_to_book(table, { sheet: "Sayfa1" });
+    XLSX.writeFile(workbook, "tablo.xlsx");
+}
