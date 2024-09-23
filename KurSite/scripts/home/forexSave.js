75a86213a41466bc5ac79bf31e4b18292dc2db0c
Onur Nergiz <nerg.onur@gmail.com>
2024-09-23 22:36:38 +0300

    forex save

--- KurSite/scripts/home/forexSave.js
+++ KurSite/scripts/home/forexSave.js
@@ -3,8 +3,10 @@
 
     function getForexCount() {
         var savedForex = JSON.parse(localStorage.getItem("savedForex"));
+        if (!savedForex == null) {
+            return 0;
+        }
         let count = Object.keys(savedForex).length;
-        console.log(count);
         return count;
     }
 
@@ -12,14 +14,14 @@
 
     $("#forexSaveButton").click(function () {
         if (getForexCount() < 10) {
-            var firstForex = $("#firstForexSelected option:selected").text();
-            var secondForex = $("#secondForexSelected option:selected").text();
+            var forex = $("#forexSaveSelect option:selected").text();
+            var value = $("#forexSaveSelect option:selected").val();
 
             var savedForex = JSON.parse(localStorage.getItem("savedForex"));
             if (savedForex == null) {
                 savedForex = {};
             }
-            savedForex[firstForex] = secondForex;
+            savedForex[forex] = value;
             localStorage.setItem("savedForex", JSON.stringify(savedForex));
             savedForexListUpdate("update");
         }
@@ -49,11 +51,8 @@ function savedForexListUpdate(selection) {
                 var html = `
             <li class="list-group-item list-group-item-action rounded-3 py-2 mb-4 shadow-sm">
                         <div class="row align-items-center">
-                            <div class="col-4">
+                            <div class="col-8">
                                 ${key}
-                            </div>
-                            <div class="col-4">
-                                ${savedForex[key]}
                             </div>
                             <div class="col-2">
                                 <button class="btn btn-danger" onclick="savedForexListDelete('${key}')"><i class="fa-solid fa-trash"></i></button>
@@ -69,6 +68,35 @@ function savedForexListUpdate(selection) {
                 $("#savedForexList").html(oldListHtml + html);
             }
         }
+        $("#savedForexRows").html("");
+        var savedForex2 = JSON.parse(localStorage.getItem("savedForex"));
+        if (savedForex2 != null) {
+            for (let key in savedForex) {
+                var oldListHtml = $("#savedForexRows").html();
+                var html = `
+            <tr>
+                                    <td>
+                                        ${key}
+                                    </td>
+                                    <td>
+                                        ${savedForex2[key]}
+                                    </td>
+                                    <td>
+                                        Çevirililen Kur Adeti
+                                    </td>
+                                    <td>
+                                        Çevirililen Kur
+                                    </td>
+                                    <td>
+                                        Değer
+                                    </td>
+                                </tr>
+
+            `;
+                $("#savedForexRows").html(oldListHtml + html);
+            }
+        }
+
     }
     else if (selection == "delete") {
         var savedForex = JSON.parse(localStorage.getItem("savedForex"));
@@ -79,11 +107,8 @@ function savedForexListUpdate(selection) {
                 var html = `
             <li class="list-group-item list-group-item-action rounded-3 py-2 mb-4 shadow-sm">
                         <div class="row align-items-center">
-                            <div class="col-4">
+                            <div class="col-8">
                                 ${key}
-                            </div>
-                            <div class="col-4">
-                                ${savedForex[key]}
                             </div>
                             <div class="col-2">
                                 <button class="btn btn-danger" onclick="savedForexListDelete('${key}')"><i class="fa-solid fa-trash"></i></button>
@@ -97,6 +122,35 @@ function savedForexListUpdate(selection) {
 
             `;
                 $("#savedForexList").html(html+oldListHtml);
+            }
+        }
+
+        $("#savedForexRows").html("");
+        var savedForex2 = JSON.parse(localStorage.getItem("savedForex"));
+        if (savedForex2 != null) {
+            for (let key in savedForex) {
+                var oldListHtml = $("#savedForexRows").html();
+                var html = `
+            <tr>
+                                    <td>
+                                        ${key}
+                                    </td>
+                                    <td>
+                                        ${savedForex2[key]}
+                                    </td>
+                                    <td>
+                                        Çevirililen Kur Adeti
+                                    </td>
+                                    <td>
+                                        Çevirililen Kur
+                                    </td>
+                                    <td>
+                                        Değer
+                                    </td>
+                                </tr>
+
+            `;
+                $("#savedForexRows").html(oldListHtml + html);
             }
         }
     }
